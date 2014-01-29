b9f55b62336a0f5948a79200e029f0c85616eac7
Robin Thunström <robin.thunstrom@gmail.com>
2014-01-29 16:55:48 +0100

    Work in progress.

---
+++ Assets/Scripts/GraphicsController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GraphicsController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyUp(KeyCode.Alpha0)) {
+			string statusOfVSYNC;
+			if(QualitySettings.vSyncCount == 0){
+				QualitySettings.vSyncCount = 1;
+				statusOfVSYNC = "ON";
+			}
+			else{
+				QualitySettings.vSyncCount = 0;
+				statusOfVSYNC = "OFF";
+			}
+			Debug.Log("VSYNC was changed to: " + statusOfVSYNC);
+		}
+	}
+}
