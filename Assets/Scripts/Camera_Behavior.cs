9e96b1fae35192d2f80d7d9252aea68cd713272a
Robin Thunström <robin.thunstrom@gmail.com>
2014-01-29 15:07:18 +0100

    Added the first commit to the repo.

---
+++ Assets/Scripts/Camera_Behavior.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_Behavior : MonoBehaviour {
+
+	public Transform interpolateTo;
+	public float interpolateSpeed = 1.0f;
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp (transform.position, interpolateTo.position, 
+		                                   Time.deltaTime * interpolateSpeed);
+		transform.rotation = Quaternion.Lerp(transform.rotation, interpolateTo.rotation, 
+		                                   Time.deltaTime * interpolateSpeed);
+	}
+}
